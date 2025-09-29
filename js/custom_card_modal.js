54acf60eb3e7265bbdc2925ef7eeb8ccc35d240f
ZinGer_KyoN <zinger.kyon@gmail.com>
2025-09-29 17:22:38 +0800

    添加自定义的游侠伙伴卡

--- js/custom_card_modal.js
+++ js/custom_card_modal.js
@@ -176,7 +176,8 @@ document.addEventListener('DOMContentLoaded', () => {
         domain: { type: '领域卡', keys: ['领域', '等级', '属性', '回想'] },
         class: { type: '主职', keys: ['领域', '初始闪避值', '初始生命点', '希望特性'] },
         subclass: { type: '子职', keys: ['主职', '等级', '施法属性'] },
-        image: { type: '图片', keys: ['imageUrl'] }
+        image: { type: '图片', keys: ['imageUrl'] },
+        beastbound: { type: '游侠伙伴', keys: ['闪避值', '伤害骰/范围'] },
     };
 
     window.customCardModal = new CustomCardModal(
